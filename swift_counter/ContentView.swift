3ae709e34775caf8618351f12a03ae870655a936
Fernando Belotto <fernando.bbosco@gmail.com>
2023-01-14 10:44:58 -0300

    Initial Commit

---
+++ swift_counter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  swift_counter
+//
+//  Created by Fernando Bosco on 14/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swift_counter/swift_counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_counterApp.swift
+//  swift_counter
+//
+//  Created by Fernando Bosco on 14/01/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
